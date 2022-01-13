a4f1d0c3d906cacaa4d4f8f932146b478308e9b7
Ruslan Razakov <privetparapet@gmail.com>
2022-01-13 22:21:06 +0300

    добавлены алерты ливенесс и матчингаю редактирование верстки

--- src/Client/wwwroot/scripts.js
+++ src/Client/wwwroot/scripts.js
@@ -1,22 +1,39 @@
-﻿function startVideo() {
+﻿var _stream;
+
+function startVideo() {
     var video = document.getElementById('video');
 
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+            _stream = stream;
+
+            _stream.stop = function () {
+                this.getAudioTracks().forEach(function (track) {
+                    track.stop();
+                });
+                this.getVideoTracks().forEach(function (track) { //in case... :)
+                    track.stop();
+                });
+            };
+
             try {
                 video.srcObject = stream;
             } catch (error) {
                 video.src = window.URL.createObjectURL(stream);
             }
-            //video.play();
+            video.play();
         });
     }
+}
+
+function stopVideo(stream) {
+    _stream.stop();
 }
 
 window.Snap = async (src, dest) => {
     let video = document.getElementById(src);
     let ctx = get2DContext(dest);
-    ctx.drawImage(video, 0, 0, 320, 240);
+    ctx.drawImage(video, 0, 0, 480, 360);
 }
 
 // Get image as base64 string
