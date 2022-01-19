3dacff1b05b39b50109c4a4e544388605f34b2ad
Ruslan Razakov <privetparapet@gmail.com>
2022-01-19 13:36:17 +0300

    исправления в JS script

--- src/Client/wwwroot/scripts.js
+++ src/Client/wwwroot/scripts.js
@@ -51,7 +51,7 @@ function recordVideoAndSendToServer(url, engineId, instance, callback) {
             if (this.readyState == 4 && this.status == 200) {
                 console.log(this.responseText);
                 // DotNet.invokeMethod('MessengerWeb.Client', 'GetLivenessFromNtech', this.responseText);
-                dotNetGlobal.DotNetReference.invokeMethod('MessengerWeb.Client', 'GetLivenessFromNtech', this.responseText);
+                dotNetGlobal.DotNetReference.invokeMethod('GetLivenessFromNtech', this.responseText);
             }
         };
 
